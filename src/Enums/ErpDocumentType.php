83b7a69fa6fa6cef59d982841f3a0514c789f859
Philipp Rappold <p.rappold@linksderisar.com>
2025-06-16 15:07:55 +0200

    Adds purchase order type

--- src/Enums/ErpDocumentType.php
+++ src/Enums/ErpDocumentType.php
@@ -5,6 +5,8 @@ namespace Robodocxs\RobodocxsMiddlewareDtos\Enums;
 enum ErpDocumentType: string
 {
     case BLANKET_ORDER = 'blanket_order';
-    case ORDER = 'order';
+    case ORDER = 'order'; // This is a sales order
     case QUOTE = 'quote';
+
+    case PURCHASE_ORDER = 'purchase_order';
 }
