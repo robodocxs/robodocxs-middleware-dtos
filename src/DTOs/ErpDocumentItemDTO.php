93b9ffd379afba9c1cc9f343ea01a9a6b179f504
Philipp Rappold <p.rappold@linksderisar.com>
2025-08-04 13:48:20 +0200

    Adds customs to ERP document items

--- src/DTOs/ErpDocumentItemDTO.php
+++ src/DTOs/ErpDocumentItemDTO.php
@@ -3,18 +3,23 @@
 namespace Robodocxs\RobodocxsMiddlewareDtos\DTOs;
 
 use Carbon\Carbon;
+use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\DataCollection;
 
 class ErpDocumentItemDTO extends Data
 {
     public function __construct(
-        public string|null     $id = null,
-        public string|null     $line_item_number = null,
-        public string|null     $name = null,
-        public float|null      $quantity = null,
-        public ProductDTO|null $product = null,
-        public Carbon|null     $valid_until = null,
-        public Carbon|null     $delivery_date = null,
+        public string|null         $id = null,
+        public string|null         $line_item_number = null,
+        public string|null         $name = null,
+        public float|null          $quantity = null,
+        public ProductDTO|null     $product = null,
+        public Carbon|null         $valid_until = null,
+        public Carbon|null         $delivery_date = null,
+
+        #[DataCollectionOf(CustomDataDTO::class)]
+        public DataCollection|null $customs = null,
     )
     {
     }
